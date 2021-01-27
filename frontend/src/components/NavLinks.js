0e77cfc9571ce47c3fdd424b28921bdf3864ae4e
Tneebs <tedneben@gmail.com>
2021-01-27 15:51:02 -0600

    working on navlink display

--- frontend/src/components/NavLinks.js
+++ frontend/src/components/NavLinks.js
@@ -4,13 +4,27 @@ import { NavLink } from "react-router-dom";
 const NavLinks = (props) => {
     return(
         <div className='navlinks-container'>
-            <div className='navlinks'>
+
+            <div className='navlink-home'>
                 <NavLink to='/' exact>Home</NavLink>
+            </div>
+
+            <div classNme='navlink-portfolio'>
                 <NavLink to='/portfolio' exact>Portfolio</NavLink>
+            </div>
+
+            <div className='navlink-blogs'>
                 <NavLink to='/blogs' exact>Blogs</NavLink>
+            </div>
+
+            <div className='navlink-resume'>
                 <NavLink to='/resume' exact>Resume</NavLink>
+            </div>
+
+            <div className='navlink-contact'> 
                 <NavLink to='/contact' exact>Contact</NavLink>
             </div>
+            
         </div>
     )
 }
