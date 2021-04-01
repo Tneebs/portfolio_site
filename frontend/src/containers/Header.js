ab0af3ef80a54b5cea80dfe6c1814c523671c4a3
Tneebs <tedneben@gmail.com>
2021-04-01 17:45:22 -0500

    Bootstrap Page Adjustments 1

--- frontend/src/containers/Header.js
+++ frontend/src/containers/Header.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import NavLinks from '../components/NavLinks';
-import NavLogo from '../components/NavLogo';
 
 const Header = (props) => {
     return (
         <div className='header'>
 
             <NavLinks />
-            <NavLogo />
 
         </div>
     )
