c8b6e0f66b4f824e4b9424064fc4a625c67d4a3e
Tneebs <tedneben@gmail.com>
2021-05-06 05:02:41 -0500

    Mobile Response Sizing

--- frontend/src/components/NavLinks.js
+++ frontend/src/components/NavLinks.js
@@ -22,25 +22,7 @@ const NavLinks = (props) => {
                 <a href="/resume" className='nav'>Resume</a>
                 <a href="/contact" className='nav'>Contact</a>
 
-            </div>
-
-            {/* <Navbar bg="light" className='navbar-links'>
-                <Navbar.Brand href="/home" className='nav-home'>
-                    <img
-                        src={TNeebsSig}
-                        width="50"
-                        height="50"
-                        className="d-inline-block align-top"
-                        alt=""
-                    />
-                </Navbar.Brand>
-                <Navbar.Brand href='/about' className='nav-about'>About Me</Navbar.Brand>
-                <Navbar.Brand href='/portfolio'>Portfolio</Navbar.Brand>
-                <Navbar.Brand href='/blogs'>Blogs</Navbar.Brand>
-                <Navbar.Brand href='/resume'>Resume</Navbar.Brand>
-                <Navbar.Brand href='/contact'>Contact</Navbar.Brand>
-            </Navbar> */}
-            
+            </div>            
         </div>
     )
 }
